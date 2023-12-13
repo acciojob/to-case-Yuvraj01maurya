7dced5bc6a6b06857bc69a3b5d1f940dadf5baf0
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-13 20:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function toCase(text) {
   // write your code here
+	if (!text) {
+    return '-';
+ }
+
+ // Convert the string to lowercase and uppercase
+ const lowerCaseStr = text.toLowerCase();
+ const upperCaseStr = text.toUpperCase();
+
+ // Return the lowercase string, uppercase string with - as the delimiter
+ return `${lowerCaseStr}-${upperCaseStr}`;
 }
 
 // DO not change the code below
